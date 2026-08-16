7940587f24b43e039e7c2578277cd2452b4f5d30
Brunel EBATA <brunelebata2@gmail.com>
2026-08-17 01:42:30 +0200

    Update WebConfig

--- apv/src/main/java/eajc/group/apv/config/WebConfig.java
+++ apv/src/main/java/eajc/group/apv/config/WebConfig.java
@@ -1,13 +1,13 @@
 package eajc.group.apv.config;
 
-import org.springframework.beans.factory.annotation.Value;
+/*import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;*/
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+/*
     @Value("${file.upload-dir}")
     private String uploadDir;
 
@@ -16,5 +16,5 @@ public class WebConfig implements WebMvcConfigurer {
         registry
                 .addResourceHandler("/uploads/**")
                 .addResourceLocations("file:///" + uploadDir + "/");
-    }
+    }*/
 }
