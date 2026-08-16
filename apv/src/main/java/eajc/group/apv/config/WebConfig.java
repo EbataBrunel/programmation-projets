3e3d4c14f0fc482b964606ca100658e5a4d03553
Brunel EBATA <brunelebata2@gmail.com>
2026-08-16 15:03:28 +0200

    Update WebConfig

--- apv/src/main/java/eajc/group/apv/config/WebConfig.java
+++ apv/src/main/java/eajc/group/apv/config/WebConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.nio.file.Paths;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -15,15 +13,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
-        String uploadLocation = Paths.get(uploadDir)
-                .toAbsolutePath()
-                .normalize()
-                .toUri()
-                .toString();
-
         registry
                 .addResourceHandler("/uploads/**")
-                .addResourceLocations(uploadLocation);
+                .addResourceLocations("file:///" + uploadDir + "/");
     }
 }
